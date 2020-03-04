917088c1b2170313d7a9a32f52bd0b189630e31f
andikas95-dev <andikaperdana95@gmail.com>
2020-03-04 11:55:11 +0700

    ganti props langsung ke nama props nya, cek Navbar & UserItem && add proptype in UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const UserItem = props => {
-    const { login, avatar_url, html_url } = props.user
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 
     return <div className="card text-center">
         <img
@@ -18,6 +18,12 @@ const UserItem = props => {
                 </a>
         </div>
     </div>
+}
+
+
+//untuk keyword cepat ketik pt = propTypes, o = object, a=array, r=isRequired
+UserItem.PropTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem;
